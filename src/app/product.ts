f4e4bfd0b6f8e6759d71e0870b706adb1096c413
Akihiro Iwata <iakihiro22@gmail.com>
2020-07-09 01:16:49 +0900

    詳細画面と一覧画面について、動的に表示できるようにした

--- src/app/product.ts
+++ src/app/product.ts
@@ -1,18 +1,59 @@
 export const products = [
-    {
-      name: 'Phone XL',
-      price: 799,
-      description: 'A large phone with one of the best screens'
-    },
-    {
-      name: 'Phone Mini',
-      price: 699,
-      description: 'A great phone with one of the best cameras'
-    },
-    {
-      name: 'Phone Standard',
-      price: 299,
-      description: ''
-    }
-  ];
-  
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    headers:[
+      {
+        name:'headName1-1',
+        description:'this is heading place.first'
+      },
+      {
+        name:'headName2',
+        description:'this is heading place.2nd'
+      },
+      {
+        name:'headName3',
+        description:'this is heading place.3rd'
+      }
+    ]
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    headers:[
+      {
+        name:'headName1-2',
+        description:'this is heading place.first'
+      },
+      {
+        name:'headName2',
+        description:'this is heading place.2nd'
+      },
+      {
+        name:'headName3',
+        description:'this is heading place.3rd'
+      }
+    ]
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: 'aaa',
+    headers:[
+      {
+        name:'headName1-3',
+        description:'this is heading place.first'
+      },
+      {
+        name:'headName2',
+        description:'this is heading place.2nd'
+      },
+      {
+        name:'headName3',
+        description:'this is heading place.3rd'
+      }
+    ]
+  }
+];
--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { products } from '../../product';
 
 @Component({
   selector: 'app-product-list',
@@ -6,12 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
-
+  products:any;
   constructor() { }
 
 
-  products:any = [1,2,3,4]
+  // products:any = [1,2,3,4]
   ngOnInit(): void {
+    this.products = products;
   }
 
 }
